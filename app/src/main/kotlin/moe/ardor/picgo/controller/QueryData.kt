a2e3335fadaed17c9c5f462653451f0c6f5bd304
ssyximmlove <ssyximmlove@gmail.com>
2021-08-12 12:30:06 +0800

    发行版1.0.1
    Signed-off-by: ssyximmlove <ssyximmlove@gmail.com>

--- app/src/main/kotlin/moe/ardor/picgo/controller/QueryData.kt
+++ app/src/main/kotlin/moe/ardor/picgo/controller/QueryData.kt
@@ -3,13 +3,14 @@ package moe.ardor.picgo.controller
 import kotlinx.coroutines.runBlocking
 import moe.peanutmelonseedbigalmond.pixiv.net.PixivSniBypassClient
 import moe.peanutmelonseedbigalmond.pixiv.response.IllustrationInfoResponse
+import moe.peanutmelonseedbigalmond.pixiv.response.ImagesInfoResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class queryData {
+class QueryData {
     @GetMapping("/illust")
     fun illustPage(): String {
         return "请以“/illust/{pid}({pid}改为你所查询图片的id)”形式进行查询"
@@ -19,6 +20,14 @@ class queryData {
         return runBlocking {
             val pixivClient = PixivSniBypassClient()
             return@runBlocking pixivClient.getIllustrationInfoByPid(pid)
+        }
+
+    }
+    @GetMapping("/img/{pid}")
+    fun queryImagebyPid(@PathVariable("pid")pid:String): ImagesInfoResponse {
+        return runBlocking {
+            val pixivClient = PixivSniBypassClient()
+            return@runBlocking pixivClient.getIllustrationImagesByPid(pid)
         }
 
     }
